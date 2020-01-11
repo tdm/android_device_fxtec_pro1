98244c496bc19a15d8c284f4b5bdf3c6c6b6bdd5
Pranav Vashi <neobuddy89@gmail.com>
2020-01-12 00:37:57 +0530

    pro1: Skip interfaces that are buildable (XXXTDM: check this)
    Change-Id: I6b5fd4627e622ada5fbdbd4ebfd287108f82a510
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -205,8 +205,7 @@ PRODUCT_PACKAGES += \
     init.qcom.rc
 
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0_vendor
+    vendor.qti.hardware.camera.device@1.0
 
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
@@ -242,8 +241,7 @@ PRODUCT_PACKAGES += \
     libvulkan
 
 PRODUCT_PACKAGES += \
-    vendor.display.config@1.0 \
-    vendor.display.config@1.0_vendor
+    vendor.display.config@1.0
 
 # Doze
 PRODUCT_PACKAGES += \
