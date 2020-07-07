570ea4d53b4bb3f3aefd2a07e5958c5612621160
Tom Marshall <tdm.code@gmail.com>
2020-07-06 20:19:17 -0700

    Initial change from oneplus/msm8998-common to fxtec/pro1

--- livedisplay/service.cpp
+++ livedisplay/service.cpp
@@ -14,131 +14,39 @@
  * limitations under the License.
  */
 
-#include <dlfcn.h>
-
-#define LOG_TAG "vendor.lineage.livedisplay@2.0-service.oneplus_msm8998"
+#define LOG_TAG "lineage.livedisplay@2.0-service.idealte_msm8998"
 
 #include <android-base/logging.h>
 #include <binder/ProcessState.h>
 #include <hidl/HidlTransportSupport.h>
-
 #include "DisplayModes.h"
-#include "PictureAdjustment.h"
-
-#define SDM_DISP_LIB "libsdm-disp-vndapis.so"
-
-using android::OK;
-using android::sp;
-using android::status_t;
-using android::hardware::configureRpcThreadpool;
-using android::hardware::joinRpcThreadpool;
+#include "SunlightEnhancement.h"
 
 using ::vendor::lineage::livedisplay::V2_0::IDisplayModes;
-using ::vendor::lineage::livedisplay::V2_0::IPictureAdjustment;
+using ::vendor::lineage::livedisplay::V2_0::ISunlightEnhancement;
 using ::vendor::lineage::livedisplay::V2_0::implementation::DisplayModes;
-using ::vendor::lineage::livedisplay::V2_0::implementation::PictureAdjustment;
+using ::vendor::lineage::livedisplay::V2_0::implementation::SunlightEnhancement;
 
 int main() {
-    // Vendor backend
-    void* libHandle = nullptr;
-    int32_t (*disp_api_init)(uint64_t*, uint32_t) = nullptr;
-    int32_t (*disp_api_deinit)(uint64_t, uint32_t) = nullptr;
-    uint64_t cookie = 0;
+    android::sp<IDisplayModes> modesService = new DisplayModes();
+    android::sp<ISunlightEnhancement> sreService = new SunlightEnhancement();
 
-    // HIDL frontend
-    sp<DisplayModes> dm;
-    sp<PictureAdjustment> pa;
+    android::hardware::configureRpcThreadpool(2, true /*callerWillJoin*/);
 
-    status_t status = OK;
-
-    android::ProcessState::initWithDriver("/dev/vndbinder");
-
-    LOG(INFO) << "LiveDisplay HAL service is starting.";
-
-    libHandle = dlopen(SDM_DISP_LIB, RTLD_NOW);
-    if (libHandle == nullptr) {
-        LOG(ERROR) << "Can not get " << SDM_DISP_LIB << " (" << dlerror() << ")";
-        goto shutdown;
+    if (modesService->registerAsService() != android::OK) {
+        LOG(ERROR) << "Cannot register display modes HAL service.";
+        return 1;
     }
 
-    disp_api_init =
-        reinterpret_cast<int32_t (*)(uint64_t*, uint32_t)>(dlsym(libHandle, "disp_api_init"));
-    if (disp_api_init == nullptr) {
-        LOG(ERROR) << "Can not get disp_api_init from " << SDM_DISP_LIB << " (" << dlerror() << ")";
-        goto shutdown;
+    if (sreService->registerAsService() != android::OK) {
+        LOG(ERROR) << "Cannot register sunlight enhancement HAL service.";
+        return 1;
     }
 
-    disp_api_deinit =
-        reinterpret_cast<int32_t (*)(uint64_t, uint32_t)>(dlsym(libHandle, "disp_api_deinit"));
-    if (disp_api_deinit == nullptr) {
-        LOG(ERROR) << "Can not get disp_api_deinit from " << SDM_DISP_LIB << " (" << dlerror()
-                   << ")";
-        goto shutdown;
-    }
+    LOG(INFO) << "LiveDisplay HAL service ready.";
 
-    status = disp_api_init(&cookie, 0);
-    if (status != OK) {
-        LOG(ERROR) << "Can not initialize " << SDM_DISP_LIB << " (" << status << ")";
-        goto shutdown;
-    }
+    android::hardware::joinRpcThreadpool();
 
-    // DisplayModes
-    dm = new DisplayModes();
-    if (dm == nullptr) {
-        LOG(ERROR) << "Can not create an instance of LiveDisplay HAL DisplayModes Iface, exiting.";
-        goto shutdown;
-    }
-
-    // PictureAdjustment
-    pa = new PictureAdjustment(libHandle, cookie);
-    if (pa == nullptr) {
-        LOG(ERROR)
-            << "Can not create an instance of LiveDisplay HAL PictureAdjustment Iface, exiting.";
-        goto shutdown;
-    }
-
-    if (!DisplayModes::isSupported() && !pa->isSupported()) {
-        // Backend isn't ready yet, so restart and try again
-        goto shutdown;
-    }
-
-    configureRpcThreadpool(1, true /*callerWillJoin*/);
-
-    // DisplayModes service
-    if (DisplayModes::isSupported()) {
-        status = dm->registerAsService();
-        if (status != OK) {
-            LOG(ERROR) << "Could not register service for LiveDisplay HAL DisplayModes Iface ("
-                       << status << ")";
-            goto shutdown;
-        }
-    }
-
-    // PictureAdjustment service
-    if (pa->isSupported()) {
-        status = pa->registerAsService();
-        if (status != OK) {
-            LOG(ERROR) << "Could not register service for LiveDisplay HAL PictureAdjustment Iface ("
-                       << status << ")";
-            goto shutdown;
-        }
-    }
-
-    LOG(INFO) << "LiveDisplay HAL service is ready.";
-    joinRpcThreadpool();
-    // Should not pass this line
-
-shutdown:
-    // Cleanup what we started
-    if (disp_api_deinit != nullptr) {
-        disp_api_deinit(cookie, 0);
-    }
-
-    if (libHandle != nullptr) {
-        dlclose(libHandle);
-    }
-
-    // In normal operation, we don't expect the thread pool to shutdown
-    LOG(ERROR) << "LiveDisplay HAL service is shutting down.";
+    LOG(ERROR) << "LiveDisplay HAL service failed to join thread pool.";
     return 1;
 }
